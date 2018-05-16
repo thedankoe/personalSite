aa8a9c8569f5416a6173f17c89222fcc7f00057e
Daniel Koenigslieb <Daniel@Daniels-MacBook-Pro.local>
2018-05-16 12:20:31 -0700

    added blog view, mobile responsive

--- app.js
+++ app.js
@@ -72,6 +72,10 @@ app.post('/', (req, res) => {
   });
 });
 
+app.get('/blog', (req,res) => {
+  res.render('blog');
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server has started");
 });
