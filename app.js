a89f2bc01bb970dd657f3c67a221db3a2c4ace80
Daniel Koenigslieb <Daniel@Daniels-MacBook-Pro.local>
2018-05-19 12:00:18 -0700

    more responsive work

--- app.js
+++ app.js
@@ -82,10 +82,10 @@ app.get('/blog/5-web-design-trends-in-2018', (req, res) => {
   res.render('5-web-design-trends-in-2018');
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Server has started");
-});
-
-// app.listen(port, () => {
-//   console.log('Server started on http://localhost:' + port);
+// app.listen(process.env.PORT, process.env.IP, function(){
+//    console.log("Server has started");
 // });
+
+app.listen(port, () => {
+  console.log('Server started on http://localhost:' + port);
+});
